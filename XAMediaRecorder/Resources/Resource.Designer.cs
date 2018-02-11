30a959402cdaaf425b15ab887c03bc2811781827
u-jit <35623458+u-jit@users.noreply.github.com>
2018-02-11 20:05:39 +0900

    ハンズオン用ファイルを追加

--- XAMediaRecorder/Resources/Resource.Designer.cs
+++ XAMediaRecorder/Resources/Resource.Designer.cs
@@ -41,11 +41,92 @@ namespace XAMediaRecorder
 			}
 		}
 		
-		public partial class Layout
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int activity_horizontal_margin = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int activity_vertical_margin = 2131099649;
+			
+			// aapt resource value: 0x7f060007
+			public const int horizontal_page_margin = 2131099655;
+			
+			// aapt resource value: 0x7f060006
+			public const int margin_huge = 2131099654;
+			
+			// aapt resource value: 0x7f060005
+			public const int margin_large = 2131099653;
+			
+			// aapt resource value: 0x7f060004
+			public const int margin_medium = 2131099652;
+			
+			// aapt resource value: 0x7f060003
+			public const int margin_small = 2131099651;
+			
+			// aapt resource value: 0x7f060002
+			public const int margin_tiny = 2131099650;
+			
+			// aapt resource value: 0x7f060008
+			public const int vertical_page_margin = 2131099656;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
+			{
+			}
+		}
+		
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int ic_launcher = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int tile = 2130837505;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f070001
+			public const int button_capture = 2131165185;
+			
+			// aapt resource value: 0x7f070000
+			public const int surface_view = 2131165184;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int sample_main = 2130903041;
 			
 			static Layout()
 			{
@@ -60,8 +141,14 @@ namespace XAMediaRecorder
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int btnCapture = 2130968578;
+			
+			// aapt resource value: 0x7f040001
+			public const int intro_message = 2130968577;
 			
 			static String()
 			{
@@ -69,6 +156,37 @@ namespace XAMediaRecorder
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050002
+			public const int AppTheme = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int Theme_Base = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int Theme_Sample = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int Widget = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int Widget_SampleMessage = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int Widget_SampleMessageTile = 2131034117;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
